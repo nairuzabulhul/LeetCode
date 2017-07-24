f82af6b6e8915f006c9a712a06b99ea8a5f982d1
Nairuz.Abulhul <nairuz.abulhul@gmail.com>
2017-07-24 08:08:46 -0400

    Update TwoSums.py

--- Arrays/TwoSums.py
+++ Arrays/TwoSums.py
@@ -1,1 +1,28 @@
 
+
+
+def two_sums (arr, target):
+  
+  look_dict = {}
+  
+  for key, valu in enumerate(arr):
+    
+    res = target - value 
+    
+    if res in look_dict:
+      return [look_dict[value]. i ]
+
+    else:
+      
+      look_dict[value] = i 
+      
+      
+      
+      
+arr = [1,5,6,3]
+target = 7
+
+Output :
+  
+ [0,2]
+
